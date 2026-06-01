cc1d9ad40b554ac245f30872e271188c70a2c69e
惊异能天使 <3605657098@qq.com>
2026-06-01 09:26:34 +0800

    完成了匹配的实体类

--- models/matching_weight.py
+++ models/matching_weight.py
@@ -1,12 +1,11 @@
-from sqlalchemy import Column, BIGINT, String, DECIMAL, DateTime, func
+from sqlalchemy import Column, BIGINT, DECIMAL, DateTime, func, ForeignKey
 from db.database import Base
 
 class MatchingWeight(Base):
     __tablename__ = "matching_weight"
 
     id = Column(BIGINT, primary_key=True, autoincrement=True)
-    job_type = Column(String(50), unique=True, nullable=False, comment="岗位类别")
-    job_type_desc = Column(String(100), comment="描述")
+    category_id = Column(BIGINT, ForeignKey("job_category.id"), nullable=False, comment="关联类别ID")
     skill_weight = Column(DECIMAL(3,2), nullable=False, comment="技能权重")
     experience_weight = Column(DECIMAL(3,2), nullable=False, comment="经验")
     education_weight = Column(DECIMAL(3,2), nullable=False, comment="学历")
