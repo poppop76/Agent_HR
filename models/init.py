a3d0a59636ac5464f447d0afac19f1088c955a79
惊异能天使 <3605657098@qq.com>
2026-06-09 00:26:42 +0800

    删除了一些无用文件文件

--- models/init.py
+++
@@ -1,9 +0,0 @@
-from models.user import SysUser
-from models.job import Job
-from models.matching_weight import MatchingWeight
-from models.resume import Resume
-from models.candidate import Candidate
-from models.matching_task import MatchingTask
-from models.matching_result import MatchingResult
-from models.ai_report_record import AiReportRecord
-from agent.memory.memory_manager import ConversationMemory, ConversationSession
