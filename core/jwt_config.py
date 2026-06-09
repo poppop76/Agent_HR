fba79dd01830f7c0039a32b7220ec65772dbcc56
惊异能天使 <3605657098@qq.com>
2026-06-09 17:13:19 +0800

    修改了JWT令牌的配置

--- core/jwt_config.py
+++ core/jwt_config.py
@@ -26,7 +26,7 @@ def get_current_user(token: str = Depends(oauth2_scheme)):
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
-        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        payload = jwt.decode(token, settings.JWT_KEY, algorithms=[settings.ALGORITHM])
         username: str = payload.get("sub")
         if username is None:
             raise credentials_exception
